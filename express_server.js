9a0f1d5877e7406005d05cd39273b38efc454ac5
fayezosaadi <fayezosaadi@gmail.com>
2017-04-14 15:48:40 +0000

    user loging and registration code

--- express_server.js
+++ express_server.js
@@ -2,33 +2,33 @@
   const app = express();
   const PORT = process.env.PORT || 8080; // default port 8080
   const bodyParser = require("body-parser");// middleware
-  const cookieParser = require('cookie-parser') // middleware
+  var cookieSession = require('cookie-session');
   const findUserByEmail = require('./helpers/finduserbyemail')
   const findUserByPass = require('./helpers/matchpassword')
-
+  const bcrypt = require('bcrypt');
   app.use(bodyParser.urlencoded({extended: true}));
 
-  app.use(cookieParser())
+  app.use(cookieSession({
+    name: 'session',
+    keys: ['lighthouse'],
+    maxAge: 24 * 60 * 60 * 1000
+  }));
 
   app.set("view engine", "ejs")
 
-
-  //A function that produces a string of 6 random alphanumeric characters- Used a url shortener
   function generateRandomString() {
     return Math.random().toString(36).substr(2, 6);
   }
 
-  let urlDatabase = {
-    "b2xVn2": "http://www.lighthouselabs.ca",
-    "9sm5xK": "http://www.google.com"
-  };
+  //URLs DB
+  let urlDatabase = {};
 
-  //Temp Users DB
   let users = {
     "userRandomID": {
       id: "userRandomID",
       email: "user@example.com",
       password: "purple-monkey-dinosaur"
+
     },
    "user2RandomID": {
       id: "user2RandomID",
@@ -36,60 +36,58 @@
       password: "dishwasher-funk"
     }
   }
+
   app.get("/", (req, res) => {
     res.redirect("/urls");
   });
-
 
   app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
   });
 
-  app.get("/hello", (req, res) => {
-    res.end("<html><body>Hello <b>World</b></body></html>\n");
-  });
-
   app.get("/urls", (req, res) => {
-    let templateVars = { urls: urlDatabase, user: users[req.cookies.user_id]};
-    res.render("urls_index", templateVars);
+    let templateVars = { urls: urlDatabase, user: users[req.session.user_id]};
+    console.log(templateVars.user);
+    if (!req.session['user_id']){
+      res.redirect("/login")
+    }
+    else{ res.render("urls_index", templateVars);
+    }
   });
 
-  //Edit button feature
   app.get("/urls/id", (req, res) => {
     res.redirect("/urls/new");
   });
 
-
   app.get("/urls/new", (req, res) => {
-    let templateVars = { user: users[req.cookies.user_id]};
-    res.render("urls_new", templateVars);
+    let templateVars = { user: users[req.session.user_id]};
+    if (!req.session.user_id){
+      res.redirect("/login")
+    }
+    else {
+      res.render("urls_new", templateVars);
+    }
   });
 
-
-  // POST Response to Update the Resourse
   app.post("/urls/:id", (req, res) => {
     urlDatabase[req.params.id] = req.body["longURL"];
     res.redirect("/urls");
   });
 
-
   app.get("/urls/:id", (req, res) => {
-    let longURL = urlDatabase[req.params.id]
-    let templateVars = { shortURL: req.params.id, longU: longURL, user: users[req.cookies.user_id]};
+    let longURL = urlDatabase[req.params.id].longURL
+    let templateVars = { shortURL: req.params.id, longU: longURL, user: users[req.session.user_id]};
     res.render("urls_show", templateVars);
   });
 
-
   app.post("/urls", (req, res) => {
-    // console.log(req.body);  // debug statement to see POST parameters
-    // res.send("Ok");         // Respond with 'Ok' (we will replace this)
     let shortUrl = generateRandomString();
-    urlDatabase[shortUrl] = req.body["longURL"];
-    res.redirect("/urls/" + shortUrl);
+    urlDatabase[shortUrl] = {longURL: req.body["longURL"], shortURL: shortUrl, user_id: req.session.user_id};
+    res.redirect('/urls');
   });
 
   app.get("/u/:shortURL", (req, res) => {
-    let longURL = urlDatabase[req.params.shortURL];
+    let longURL = urlDatabase[req.params.shortURL].longURL;
     if(longURL) {
       res.redirect(longURL);
     } else {
@@ -103,49 +101,54 @@
   });
 
   app.post("/login", (req, res) => {
-    if ( req.body.email && req.body.password ){
-      if (!findUserByEmail(users, req.body.email)){
+    const email = req.body.email;
+    const password = req.body.password;
+    console.log(email + " " + password);
+    if (email && password) {
+    var user = findUserByEmail(users, email);
+      if (!findUserByEmail(users, email)){
         res.status(403).send('this user is not registered');
       }
-      else if (!findUserByPass(users, req.body.password)){
+      else if (!bcrypt.compareSync(password, user.password)){
         res.status(403).send('password is incorrect');
       }
       else {
-        let id = Object.keys(users).find(key => users[key].email === req.body.email);
-        res.cookie('user_id', id);
+        let id = Object.keys(users).find(key => users[key].email === email);
+        req.session.user_id = id;
         res.redirect('/');
       }
       }
     else {
       res.status(404).send('you must have an email address or password to use this service');
     }
-
   });
+
   app.get("/login", (req, res) => {
     res.render('urls_login');
   });
 
   app.get("/logout", (req, res) => {
-    res.clearCookie('user_id');
+    req.session = null;
     res.render('urls_login');
   });
 
-  // Get response for user regestration
   app.get("/register", (req, res) => {
     res.render("urls_register");
   });
 
-  // POST Response for user Registration, with the Error Massages
   app.post("/register", (req, res) => {
     if ( req.body.email && req.body.password ){
       if (findUserByEmail(users, req.body.email)){
         res.status(400).send('this user is already registered');
       }
       else {
+        let password = req.body.password;
+        let hashed_password = bcrypt.hashSync(password, 10);
         let randomId = generateRandomString();
-        users[randomId] = {id: randomId, email: req.body.email, password: req.body.password};
-        res.cookie('user_id', randomId );
-        res.redirect('/');
+        users[randomId] = {id: randomId, email: req.body.email, password: hashed_password};
+        console.log(users)
+        req.session.user_id = randomId;
+        res.redirect('urls');
       }
     }
     else {
