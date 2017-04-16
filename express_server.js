68548698b2dff4bcded8c5e951f493839dfdc574
fayezosaadi <fayezosaadi@gmail.com>
2017-04-16 18:44:58 +0000

    Code Refactoring

--- express_server.js
+++ express_server.js
@@ -1,7 +1,7 @@
   const express = require("express");
   const app = express();
-  const PORT = process.env.PORT || 8080; // default port 8080
-  const bodyParser = require("body-parser");// middleware
+  const PORT = process.env.PORT || 3000;
+  const bodyParser = require("body-parser");
   var cookieSession = require('cookie-session');
   const findUserByEmail = require('./helpers/finduserbyemail')
   const findUserByPass = require('./helpers/matchpassword')
@@ -38,61 +38,93 @@
   }
 
   app.get("/", (req, res) => {
-    res.redirect("/urls");
-  });
-
-  app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
-  });
-
-  app.get("/urls", (req, res) => {
-    let templateVars = { urls: urlDatabase, user: users[req.session.user_id]};
-    console.log(templateVars.user);
     if (!req.session['user_id']){
       res.redirect("/login")
     }
-    else{ res.render("urls_index", templateVars);
+    else{
+      res.redirect("/urls");
     }
   });
 
-  app.get("/urls/id", (req, res) => {
-    res.redirect("/urls/new");
+  app.get("/urls", (req, res) => {
+    if (!req.session['user_id']){
+      res.status(401);
+      res.render('urls_401');
+    }
+    else{
+      let templateVars = { urls: urlDatabase, user: users[req.session.user_id]};
+      res.status(200);
+      res.render("urls_index", templateVars);
+    }
   });
 
   app.get("/urls/new", (req, res) => {
     let templateVars = { user: users[req.session.user_id]};
     if (!req.session.user_id){
-      res.redirect("/login")
-    }
-    else {
+      res.status(401);
+      res.render('urls_401');
+    } else {
+      res.status(200);
       res.render("urls_new", templateVars);
     }
   });
 
   app.post("/urls/:id", (req, res) => {
-    urlDatabase[req.params.id] = req.body["longURL"];
-    res.redirect("/urls");
+    if (!urlDatabase[req.params.id]){
+      res.status(404);
+      res.render('urls_404');
+    } else if (!req.session.user_id){
+       res.status(401);
+       res.render('urls_401');
+    } else if (req.session.user_id !== urlDatabase[req.params.id].user_id){
+       res.status(403);
+       res.render('urls_403');
+    } else {
+       urlDatabase[req.params.id].longURL = req.body["longURL"];
+       res.redirect('/urls/'+ req.params.id);
+    }
   });
 
   app.get("/urls/:id", (req, res) => {
-    let longURL = urlDatabase[req.params.id].longURL
-    let templateVars = { shortURL: req.params.id, longU: longURL, user: users[req.session.user_id]};
-    res.render("urls_show", templateVars);
+    if (!urlDatabase[req.params.id]){
+      res.status(404);
+      res.render('urls_404');
+    }
+    else if (!req.session.user_id){
+     res.status(401);
+     res.render('urls_401');
+    }
+    else if (req.session.user_id !== urlDatabase[req.params.id].user_id){
+    res.status(403);
+    res.render('urls_403');
+    }
+    else{
+      let longURL = urlDatabase[req.params.id].longURL;
+      let templateVars = { shortURL: req.params.id, longU: longURL, user: users[req.session.user_id]};
+      res.status(200);
+      res.render("urls_show", templateVars);
+    }
   });
 
   app.post("/urls", (req, res) => {
+    if (!req.session.user_id){
+      res.status(401);
+      res.render('urls_401');
+    }
+    else{
     let shortUrl = generateRandomString();
     urlDatabase[shortUrl] = {longURL: req.body["longURL"], shortURL: shortUrl, user_id: req.session.user_id};
-    res.redirect('/urls');
+    res.redirect('/urls/'+ shortUrl);
+    }
   });
 
   app.get("/u/:shortURL", (req, res) => {
-    let longURL = urlDatabase[req.params.shortURL].longURL;
-    if(longURL) {
-      res.redirect(longURL);
+    if(!urlDatabase[req.params.shortURL]) {
+    res.status(404).send('Not found');
     } else {
-      res.status(404).send('Not found');
-    }
+    let longURL = urlDatabase[req.params.shortURL].longURL;
+    res.redirect(longURL);
+  }
   });
 
   app.post("/urls/:id/delete", (req, res) => {
@@ -103,28 +135,31 @@
   app.post("/login", (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
-    console.log(email + " " + password);
     if (email && password) {
     var user = findUserByEmail(users, email);
       if (!findUserByEmail(users, email)){
-        res.status(403).send('this user is not registered');
-      }
-      else if (!bcrypt.compareSync(password, user.password)){
-        res.status(403).send('password is incorrect');
-      }
-      else {
+        res.status(401);
+        res.render('urls_user_not_found');
+      } else if (!bcrypt.compareSync(password, user.password)){
+        res.status(401);
+        res.render('urls_user_not_found');
+      } else {
         let id = Object.keys(users).find(key => users[key].email === email);
         req.session.user_id = id;
         res.redirect('/');
       }
-      }
-    else {
+    } else {
       res.status(404).send('you must have an email address or password to use this service');
     }
   });
 
   app.get("/login", (req, res) => {
-    res.render('urls_login');
+    if (req.session.user_id) {
+      res.redirect('/');
+    } else {
+      res.status(200);
+      res.render('urls_login');
+    }
   });
 
   app.get("/logout", (req, res) => {
@@ -133,7 +168,12 @@
   });
 
   app.get("/register", (req, res) => {
-    res.render("urls_register");
+     if (req.session.user_id) {
+      res.redirect('/');
+    } else {
+      res.status(200);
+      res.render("urls_register");
+    }
   });
 
   app.post("/register", (req, res) => {
@@ -148,7 +188,7 @@
         users[randomId] = {id: randomId, email: req.body.email, password: hashed_password};
         console.log(users)
         req.session.user_id = randomId;
-        res.redirect('urls');
+        res.redirect('/');
       }
     }
     else {
